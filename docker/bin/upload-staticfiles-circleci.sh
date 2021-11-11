f00ca41c2e8fd1eda674a5161d91b9694b72456a
Leo McArdle <lmcardle@mozilla.com>
2021-11-11 16:47:26 +0000

    upload assets to s3
    only stage hashed assets, for now, to avoid breaking anything

---
+++ docker/bin/upload-staticfiles-circleci.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -exo pipefail
+
+GIT_COMMIT=${GIT_COMMIT:-latest}
+GIT_COMMIT_SHORT=${GIT_COMMIT_SHORT:-$GIT_COMMIT}
+CONTAINER_NAME="kitsune-static-${GIT_COMMIT}"
+IMAGE_NAME="mozilla/kitsune:prod-${GIT_COMMIT_SHORT}"
+TMP_DIR="s3-static"
+TMP_DIR_HASHED="s3-static-hashed"
+
+rm -rf "${TMP_DIR}"
+rm -rf "${TMP_DIR_HASHED}"
+
+# extract the static files
+docker create --name "${CONTAINER_NAME}" "${IMAGE_NAME}"
+docker cp "${CONTAINER_NAME}:/app/static" "${TMP_DIR}"
+docker rm -f "${CONTAINER_NAME}"
+
+# separate the hashed files into another directory
+docker/bin/move_hashed_staticfiles.py "${TMP_DIR}" "${TMP_DIR_HASHED}"
+
+# hashed filenames
+aws s3 sync \
+    --no-progress \
+    --acl public-read \
+    --cache-control "max-age=315360000, public, immutable" \
+    "./${TMP_DIR_HASHED}" "s3://mozit-sumo-stage-media/static/"
+
+rm -rf "${TMP_DIR}"
+rm -rf "${TMP_DIR_HASHED}"
